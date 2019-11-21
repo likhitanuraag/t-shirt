5644ef676f242f976fff3915ce84482a187c36ae
PJC-max <57852943+PJC-max@users.noreply.github.com>
2019-11-21 18:18:23 +1100

    Button matching
    Fixed buttons for next and previous slide shows for slider. Figuring out automatic slider. For now, manual slider. There are bugs that need to be fixed with image and container when in mobile version.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const carouselSlider = document.querySelector('.carousel-slider');
-const carouselImages = document.querySelectorAll('.carousel-slider img'); 
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img'); 
 
 //Button
 const prevBtn = document.querySelector('#prevBtn');
@@ -9,35 +9,35 @@ const nextBtn = document.querySelector('#nextBtn');
 let counter = 1;
 const size = carouselImages[0].clientWidth;
 
-carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 //Button listener
 nextBtn.addEventListener('click', function() {
     if(counter >= carouselImages.length -1) return;
-    carouselSlider.style.transition = "transform 0.3s ease-in-out";
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter++;
-    carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 });
 
 prevBtn.addEventListener('click', function() {
     if (counter <= 0) return;
-    carouselSlider.style.transition = "transform 0.4s ease-in-out";
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter--;
-    carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 });
 
-carouselSlider.addEventListener('transitionend', function() {
+carouselSlide.addEventListener('transitionend', function() {
     console.log(carouselImages[counter]);
     if(carouselImages[counter].id === 'lastClone'){
-        carouselSlider.style.transform = "none"; 
+        carouselSlide.style.transform = "none"; 
         counter = carouselImages.length -2;
-        carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
     if(carouselImages[counter].id === 'firstClone'){
-        carouselSlider.style.transform = "none"; 
+        carouselSlide.style.transform = "none"; 
         counter = carouselImages.length - counter;
-        carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
 });
