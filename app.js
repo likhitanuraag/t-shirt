a8381acc4f697c434f5aea19bca9ce978ff80e8f
PJC-max <57852943+PJC-max@users.noreply.github.com>
2019-11-21 22:24:47 +1100

    made changes to slider
    transition removed. Helps with smoothness. Debugging fixed.

--- app.js
+++ app.js
@@ -31,12 +31,12 @@ prevBtn.addEventListener('click', function() {
 carouselSlide.addEventListener('transitionend', function() {
     console.log(carouselImages[counter]);
     if(carouselImages[counter].id === 'lastClone'){
-        carouselSlide.style.transform = "none"; 
+        carouselSlide.style.transition = "none"; 
         counter = carouselImages.length -2;
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
     if(carouselImages[counter].id === 'firstClone'){
-        carouselSlide.style.transform = "none"; 
+        carouselSlide.style.transition = "none"; 
         counter = carouselImages.length - counter;
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
