1f32b5d1ef95e5781ffcab7fe1783087b21d38c0
PJC-max <57852943+PJC-max@users.noreply.github.com>
2019-11-21 14:48:28 +1100

    Changes to fit image container
    Containers contain sliding images. Fixed bugs in the code.

--- app.js
+++ app.js
@@ -12,9 +12,32 @@ const size = carouselImages[0].clientWidth;
 carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 //Button listener
-nextBtn.addEventListener('click',function(){
-    carouselSlider.style.transition = "transform 0.4s ease-in-out";
+nextBtn.addEventListener('click', function() {
+    if(counter >= carouselImages.length -1) return;
+    carouselSlider.style.transition = "transform 0.3s ease-in-out";
     counter++;
     carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
+});
+
+prevBtn.addEventListener('click', function() {
+    if (counter <= 0) return;
+    carouselSlider.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+});
+
+carouselSlider.addEventListener('transitionend', function() {
+    console.log(carouselImages[counter]);
+    if(carouselImages[counter].id === 'lastClone'){
+        carouselSlider.style.transform = "none"; 
+        counter = carouselImages.length -2;
+        carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if(carouselImages[counter].id === 'firstClone'){
+        carouselSlider.style.transform = "none"; 
+        counter = carouselImages.length - counter;
+        carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
 });
